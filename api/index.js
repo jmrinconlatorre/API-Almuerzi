983a17f2cf2324f0977e54f1841d6a8965134b57
Juan Manuel Rincón Latorre <jm.rincon.latorre@gmail.com>
2020-04-26 20:16:49 +0200

    added .env and mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 
+
 app.get('*', (req,res) => {
     console.log('Hola mundo! Mi primera app serverless');
     res.send({ mensaje: "Hola que tal?"});
